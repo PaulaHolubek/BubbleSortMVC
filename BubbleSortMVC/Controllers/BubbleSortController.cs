93e88e8ba494de8b80f133b2a72129ca979b3ed7
Paula H <42469219+PaulaHolubek@users.noreply.github.com>
2018-10-26 12:35:07 -0400

    Add files via upload

---
+++ BubbleSortMVC/Controllers/BubbleSortController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BubbleSortMVC.Controllers
+{
+    public class BubbleSortController : Controller
+    {
+        Random rnd = new Random();
+
+        // GET: BubbleSort
+        public ActionResult Index()
+        {
+            int[] numbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = rnd.Next(10);
+            }
+            ViewBag.MyNums = BubbleSort(numbers);
+            return View();
+        }
+
+        static int[] BubbleSort(int[] numbers)
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        //swap
+                        int temp = numbers[i + 1];
+                        numbers[i + 1] = numbers[i];
+                        numbers[i] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped == true);
+            return numbers;
+        }
+    }
+}
